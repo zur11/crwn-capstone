f91039f48f4e2c99b376e287a821718772a7f85f
zur11 <cibka11@gmail.com>
2022-03-08 14:54:15 -0500

    3 Styling menu-item done, S4 V81

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -28,14 +28,14 @@ class Directory extends React.Component {
                   linkUrl: 'shop/sneakers'
                 },
                 {
-                  title: 'womens',
+                  title: "women's",
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
                   linkUrl: 'shop/womens'
                 },
                 {
-                  title: 'mens',
+                  title: "men's",
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
