8483c64124019adedbae1c2e41252a304b32ef85
zur11 <cibka11@gmail.com>
2022-03-11 01:15:01 -0500

    5 withRouter, HOC & Props spreading S5 V88

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {withRouter} from 'react-router-dom' //* Higher Order Component (HOC)
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
 
         <div 
             className="background-image" 
@@ -19,4 +20,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //* Component access to Router Props
