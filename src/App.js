5354124f30999fe0428dae69ae360893f31273e9
zur11 <cibka11@gmail.com>
2022-03-15 17:21:03 -0500

    4 Directory comp done - S5 V89

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
-import CategoryItem from './components/category-item/category-item.component';
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
 
@@ -33,13 +32,8 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+    <Directory categories={categories} />
+  )
 }
 
 export default App;
