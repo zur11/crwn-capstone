41861a7a9d3900b27ef35958446d1f01e17c840d
zur11 <cibka11@gmail.com>
2022-03-17 04:30:41 -0500

    7 Link from RRD & Fragment - Nav + Home & Shop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 
-import {Routes, Route, Outlet} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 
-import Home from './router/home/home.component'
+import Home from './routes/home/home.component';
+import Navigation from "./routes/navigation/navigation.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the Navigation Bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
